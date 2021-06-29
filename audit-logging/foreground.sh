7920ec76b1574ef134e2607d685fab23b3834937
drchung5 <dchung@dchung.com>
2021-06-29 06:01:37 +0000

    move script to foreground

--- audit-logging/foreground.sh
+++ audit-logging/foreground.sh
@@ -1,2 +1,10 @@
 # #!/bin/bash
-# sleep 8; wait.sh; source .bashrc
+
+echo "deb http://downloads.apache.org/cassandra/debian 40x main" | sudo tee -a /etc/apt/sources.list.d/cassandra.sources.list
+deb http://downloads.apache.org/cassandra/debian 40x main
+
+curl https://downloads.apache.org/cassandra/KEYS | sudo apt-key add -
+
+sudo apt-get update
+
+sudo apt-get install cassandra
