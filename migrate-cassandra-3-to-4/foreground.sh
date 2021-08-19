ceb638364d5890930d58fb4b7f5822e176c38cbb
drchung5 <dchung@dchung.com>
2021-08-19 14:52:35 +0000

    removed EXPORT JAVA_HOME

--- migrate-cassandra-3-to-4/foreground.sh
+++ migrate-cassandra-3-to-4/foreground.sh
@@ -6,7 +6,6 @@ wget https://archive.apache.org/dist/cassandra/3.11.9/apache-cassandra-3.11.9-bi
 tar xzf apache-cassandra-3.11.9-bin.tar.gz
 mv apache-cassandra-3.11.9 /usr/share/cassandra
 rm apache-cassandra-3.11.9-bin.tar.gz
-export JAVA_HOME="/usr/lib/jvm/default-java"
 echo 'PATH="$PATH:/usr/share/cassandra/bin:/usr/share/cassandra/tools/bin"' >> .bashrc
 /usr/share/cassandra/bin/cassandra -R
 while [ `grep "Starting listening for CQL clients" /usr/share/cassandra/logs/system.log | wc -l` -lt 1 ]; do sleep 10; done
