5dce01af3a77946edc791dd99a9f6575635ab211
drchung5 <dchung@dchung.com>
2021-08-19 14:24:01 +0000

    initial commit

---
+++ migrate-cassandra-3-to-4/background.sh
@@ -0,0 +1,3 @@
+# !/bin/bash
+
+echo "background.sh"
---
+++ migrate-cassandra-3-to-4/foreground.sh
@@ -0,0 +1,18 @@
+# !/bin/bash
+
+echo "force downgradde Java to JDK 8"
+sudo rm -r /usr/lib/jvm
+apt-get update
+sudo apt-get -y install openjdk-8-jdk openjdk-8-jre
+
+# Install Cassandra 3.11.9
+echo "install Cassandra 3.11.9"
+wget https://archive.apache.org/dist/cassandra/3.11.9/apache-cassandra-3.11.9-bin.tar.gz
+tar xzf apache-cassandra-3.11.9-bin.tar.gz
+mv apache-cassandra-3.11.9 /usr/share/cassandra
+rm apache-cassandra-3.11.9-bin.tar.gz
+export JAVA_HOME="/usr/lib/jvm/default-java"
+echo 'PATH="$PATH:/usr/share/cassandra/bin:/usr/share/cassandra/tools/bin"' >> .bashrc
+/usr/share/cassandra/bin/cassandra -R
+while [ `grep "Starting listening for CQL clients" /usr/share/cassandra/logs/system.log | wc -l` -lt 1 ]; do sleep 10; done
+echo "done" >> /opt/katacoda-background-finished
