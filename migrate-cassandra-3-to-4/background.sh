d20a01440f712fcd894555213c04c9430196f703
drchung5 <dchung@dchung.com>
2021-08-19 15:21:10 +0000

    JAVA_HOME

--- migrate-cassandra-3-to-4/background.sh
+++ migrate-cassandra-3-to-4/background.sh
@@ -4,4 +4,3 @@ echo "force downgradde Java to JDK 8"
 sudo rm -r /usr/lib/jvm
 apt-get update
 sudo apt-get -y install openjdk-8-jdk openjdk-8-jre
-export JAVA_HOME="/usr/lib/jvm/java-1.8.0-openjdk-amd64"
