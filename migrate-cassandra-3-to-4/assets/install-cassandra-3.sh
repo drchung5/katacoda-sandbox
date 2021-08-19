e9e7c2c00eddd370a7468bedf21b11b610e040ad
drchung5 <dchung@dchung.com>
2021-08-19 16:48:10 +0000

    created separate install script

---
+++ migrate-cassandra-3-to-4/assets/install-cassandra-3.sh
@@ -0,0 +1,14 @@
+# !/bin/bash
+
+# Install Cassandra 3.11.9
+echo "install Cassandra 3.11.9"
+wget https://archive.apache.org/dist/cassandra/3.11.9/apache-cassandra-3.11.9-bin.tar.gz
+tar xzf apache-cassandra-3.11.9-bin.tar.gz
+mv apache-cassandra-3.11.9 /usr/share/cassandra
+rm apache-cassandra-3.11.9-bin.tar.gz
+export JAVA_HOME="/usr" # Casandra appends /bin/java
+export PATH="$PATH:/usr/bin:/usr/share/cassandra/bin:/usr/share/cassandra/tools/bin"
+#sleep 2
+cassandra -R # /usr/share/cassandra/bin/cassandra -R
+while [ `grep "Starting listening for CQL clients" /usr/share/cassandra/logs/system.log | wc -l` -lt 1 ]; do sleep 10; done
+echo "done" >> /opt/katacoda-background-finished
--- migrate-cassandra-3-to-4/background.sh
+++ migrate-cassandra-3-to-4/background.sh
@@ -1,2 +1,6 @@
 # !/bin/bash
 
+echo "force downgrade Java to JDK 8"
+sudo rm -r /usr/lib/jvm
+apt-get update
+sudo apt-get -y install openjdk-8-jdk openjdk-8-jre
