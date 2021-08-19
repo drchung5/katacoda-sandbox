85a5e122ed5c6fb489d8fbbce76ad105dc10532d
drchung5 <dchung@dchung.com>
2021-08-19 17:16:38 +0000

    pause

--- migrate-cassandra-3-to-4/foreground.sh
+++ migrate-cassandra-3-to-4/foreground.sh
@@ -1,10 +1,5 @@
 # !/bin/bash
 
-until [ -f /usr/bin/java ]
-do
-     sleep 5
-done
-
 # Install Cassandra 3.11.9
 echo "install Cassandra 3.11.9"
 wget https://archive.apache.org/dist/cassandra/3.11.9/apache-cassandra-3.11.9-bin.tar.gz
@@ -14,6 +9,12 @@ rm apache-cassandra-3.11.9-bin.tar.gz
 export JAVA_HOME="/usr" # Casandra appends /bin/java
 export PATH="$PATH:/usr/bin:/usr/share/cassandra/bin:/usr/share/cassandra/tools/bin"
 #sleep 2
+
+until [ -f /usr/bin/java ]
+do
+     sleep 5
+done
+
 cassandra -R # /usr/share/cassandra/bin/cassandra -R
 while [ `grep "Starting listening for CQL clients" /usr/share/cassandra/logs/system.log | wc -l` -lt 1 ]; do sleep 10; done
 echo "done" >> /opt/katacoda-background-finished
