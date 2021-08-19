4f422027bccb77a36dcca7392e64f81f645cfaf8
drchung5 <dchung@dchung.com>
2021-08-19 23:10:23 +0000

    redirect stdout

--- migrate-cassandra-3-to-4/foreground.sh
+++ migrate-cassandra-3-to-4/foreground.sh
@@ -1,8 +1,7 @@
 # !/bin/bash
-echo OFF
 echo "force downgrade Java to JDK 8"
 sudo rm -r /usr/lib/jvm
-apt-get update
+apt-get update > /dev/null 2>&1
 sudo apt-get -y install openjdk-8-jdk openjdk-8-jre < "/dev/null"
 
 until [[ -e /usr/lib/jvm/java-8-openjdk-amd64 && -e /usr/bin/java ]]
@@ -13,7 +12,7 @@ done
 
 # Install Cassandra 3.11.9
 echo "install Cassandra 3.11.9"
-wget https://archive.apache.org/dist/cassandra/3.11.9/apache-cassandra-3.11.9-bin.tar.gz
+wget https://archive.apache.org/dist/cassandra/3.11.9/apache-cassandra-3.11.9-bin.tar.gz > /dev/null 2>&1
 tar xzf apache-cassandra-3.11.9-bin.tar.gz
 mv apache-cassandra-3.11.9 /usr/share/cassandra
 rm apache-cassandra-3.11.9-bin.tar.gz
@@ -23,5 +22,4 @@ export PATH="$PATH:/usr/bin:/usr/share/cassandra/bin:/usr/share/cassandra/tools/
 cassandra -R # /usr/share/cassandra/bin/cassandra -R
 while [ `grep "Starting listening for CQL clients" /usr/share/cassandra/logs/system.log | wc -l` -lt 1 ]; do sleep 10; done
 echo "done" >> /opt/katacoda-background-finished
-echo ON 
 echo "*** Cassandra is set up ***"
