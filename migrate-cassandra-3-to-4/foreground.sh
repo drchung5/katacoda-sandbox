dc7563a05ed23e9d89b097d6bb0d159e40253295
drchung5 <dchung@dchung.com>
2021-08-20 03:36:51 +0000

    added sleep

--- migrate-cassandra-3-to-4/foreground.sh
+++ migrate-cassandra-3-to-4/foreground.sh
@@ -5,15 +5,21 @@ sudo rm -r /usr/lib/jvm
 apt-get update > /dev/null 2>&1
 sudo apt-get -y install openjdk-8-jdk openjdk-8-jre < "/dev/null" > /dev/null 2>&1 
 
+until [[ -e /usr/lib/jvm/java-8-openjdk-amd64 && -e /usr/bin/java ]]
+do
+     sleep 1
+     echo -n '.'
+done
+
 # Install Cassandra 3.11.9
 echo "install Cassandra 3.11.9"
 wget https://archive.apache.org/dist/cassandra/3.11.9/apache-cassandra-3.11.9-bin.tar.gz < "/dev/null" > /dev/null 2>&1
 tar xzf apache-cassandra-3.11.9-bin.tar.gz
 mv apache-cassandra-3.11.9 /usr/share/cassandra
 rm apache-cassandra-3.11.9-bin.tar.gz
-echo 'export JAVA_HOME="/usr"' >> $HOME/.bashrc
-echo 'export PATH="$PATH:/usr/bin:/usr/share/cassandra/bin:/usr/share/cassandra/tools/bin"' >> $HOME/.bashrc
+export JAVA_HOME="/usr"
+export PATH="$PATH:/usr/bin:/usr/share/cassandra/bin:/usr/share/cassandra/tools/bin"
 
 cassandra -R < "/dev/null" > /dev/null 2>&1
 while [ `grep "Starting listening for CQL clients" /usr/share/cassandra/logs/system.log | wc -l` -lt 1 ]; do sleep 10; done
-echo "*** Cassandra is set up ***"
+echo "*** Cassandra setup complete ***"
