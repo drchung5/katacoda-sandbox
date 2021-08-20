8c8ffb2855d21680cf65c9e4f9b86927bb4e414d
drchung5 <dchung@dchung.com>
2021-08-20 03:58:22 +0000

    echo

--- migrate-cassandra-3-to-4/foreground.sh
+++ migrate-cassandra-3-to-4/foreground.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-#
-# Downgrade Java to JDK 8
-#
+echo "Downgrade Java to JDK 8"
 
 sudo rm -r /usr/lib/jvm
 apt-get update > /dev/null 2>&1
@@ -14,9 +12,7 @@ do
      echo -n '.'
 done
 
-#
-# Install Cassandra 3.11.9
-#
+echo "Install Cassandra 3.11.9"
 
 wget https://archive.apache.org/dist/cassandra/3.11.9/apache-cassandra-3.11.9-bin.tar.gz < "/dev/null" > /dev/null 2>&1
 tar xzf apache-cassandra-3.11.9-bin.tar.gz
@@ -25,14 +21,10 @@ rm apache-cassandra-3.11.9-bin.tar.gz
 export JAVA_HOME="/usr"
 export PATH="$PATH:/usr/bin:/usr/share/cassandra/bin:/usr/share/cassandra/tools/bin"
 
-#
-# Start Cassandra
-#
+echo "Start Cassandra"
 
 cassandra -R < "/dev/null" > /dev/null 2>&1
 
 while [ `grep "Starting listening for CQL clients" /usr/share/cassandra/logs/system.log | wc -l` -lt 1 ]; do sleep 10; done
 
-#
-# Cassandra setup complete
-#
+echo "Cassandra setup complete"
